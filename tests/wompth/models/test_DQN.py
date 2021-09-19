43f89db3c7f3903e4dd58c457b5cbc1f6bb33664
Waner Miranda <wanermiranda@gmail.com>
2021-09-19 13:45:57 -0300

    Tested memory sample with a loaded batch

--- tests/wompth/models/test_DQN.py
+++ tests/wompth/models/test_DQN.py
@@ -1,5 +1,5 @@
-from wompth.models.dqn import DQN, LayerConf, ScreenDims
-
+from wompth.models.dqn import DQN, LayerConf, ReplayMemory, ScreenDims, Transition
+import torch
 
 def test_network_size():
     layout = [
@@ -17,8 +17,18 @@ def test_network_size():
         == "DQN(\n  (_Conv2d_0): Conv2d(3, 16, kernel_size=(5, 5), stride=(2, 2))\n  (_BatchNorm2d_0): BatchNorm2d(16, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)\n  (_Conv2d_1): Conv2d(16, 32, kernel_size=(5, 5), stride=(2, 2))\n  (_BatchNorm2d_1): BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)\n  (_Conv2d_2): Conv2d(32, 32, kernel_size=(5, 5), stride=(2, 2))\n  (_BatchNorm2d_2): BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)\n  (_head): Linear(in_features=512, out_features=2, bias=True)\n)"
     )
 
+def test_memory():
+    path = './resources/tests/memory.pth'
+    saved_memory = torch.load(path)
+    memory2 = ReplayMemory(128)
+    memory2.memory = saved_memory
+    transitions = memory2.sample(10)
+
+    assert len(transitions) == 10
+    assert(type(transitions[0]) == Transition)
 
 def test_forward():
+
     layout = [
         LayerConf(input=3, kernel_size=5, stride=2, batch_norm=16),
         LayerConf(input=16, kernel_size=5, stride=2, batch_norm=32),
