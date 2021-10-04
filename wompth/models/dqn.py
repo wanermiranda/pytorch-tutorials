7b0a3c49410fdf538cca96c78033d3a8bb1ab919
Waner Miranda <wanermiranda@gmail.com>
2021-10-03 21:17:08 -0300

    Playing with network architecture

--- wompth/models/dqn.py
+++ wompth/models/dqn.py
@@ -17,11 +17,9 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
 
 
-from wompth.models.base import BaseNetwork
+from wompth.models.base import BaseNetwork, NeuralNetwork
 
 Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
-
-LayerConf = namedtuple("LayerConf", ("input", "kernel_size", "stride", "batch_norm"))
 
 ScreenDims = namedtuple("ScreenDims", ("height", "width"))
 
@@ -55,94 +53,17 @@ class DQN(BaseNetwork):
         self,
         device="cuda",
         conf: DQNConf = DQNConf(),
-        layout: List[LayerConf] = [],
-        screen_dims: ScreenDims = ScreenDims(height=0, width=0),
-        outputs: float = 0,
-        activation_func=partial(F.relu),
         optimizer_partial=partial(optim.RMSprop),
         memory=ReplayMemory(10000),
+        outputs=2,
     ):
         super().__init__(device=device)
-        self._layout: List[LayerConf] = layout
-        self._screen_dims = screen_dims
-        self._outputs = outputs
-        self._activation_func = activation_func
         self._conf = conf
         self._episodes_done = 0
+        self._outputs = 2
         self._epsilon  = conf.EPS_START
         self._optimizer_partial = optimizer_partial
         self._memory = memory
-        self._compile(self._device)
-
-    def _build_stack(self):
-        stack = []
-        conv_w = None
-        conv_h = None
-        for i in range(len(self._layout)):
-            layer = self._layout[i]
-
-            if i < len(self._layout) - 1:
-                next_layer = self._layout[i + 1]
-                stack.append(
-                    nn.Conv2d(
-                        layer.input,
-                        next_layer.input,
-                        kernel_size=layer.kernel_size,
-                        stride=layer.stride,
-                    )
-                )
-            else:  # Last layer gets the output size from the batch norm size
-                stack.append(
-                    nn.Conv2d(
-                        layer.input,
-                        layer.batch_norm,
-                        kernel_size=layer.kernel_size,
-                        stride=layer.stride,
-                    )
-                )
-
-            stack.append(nn.BatchNorm2d(layer.batch_norm))
-
-            if not conv_h:  # Define the conv size based on the first layer
-                conv_w = DQN.conv2d_size_out(
-                    self._screen_dims.width,
-                    kernel_size=layer.kernel_size,
-                    stride=layer.stride,
-                )
-                conv_h = DQN.conv2d_size_out(
-                    self._screen_dims.height,
-                    kernel_size=layer.kernel_size,
-                    stride=layer.stride,
-                )
-            else:  # recursivelly calculate the conv size
-                conv_w = DQN.conv2d_size_out(
-                    conv_w, kernel_size=layer.kernel_size, stride=layer.stride
-                )
-                conv_h = DQN.conv2d_size_out(
-                    conv_h, kernel_size=layer.kernel_size, stride=layer.stride
-                )
-        self._network_stack = []
-        # Setting the network layers
-        for i, layer in enumerate(stack):
-            layer_name = f"_{type(layer).__name__}_{i//2}"
-            setattr(self, layer_name, layer)
-            self._network_stack.append(layer_name)
-
-        self._linear_input_size = conv_w * conv_h * self._layout[-1].batch_norm
-        self._head = nn.Linear(self._linear_input_size, self._outputs)
-
-    @staticmethod
-    def conv2d_size_out(size, kernel_size=5, stride=2):
-        return (size - (kernel_size - 1) - 1) // stride + 1
-
-    def forward(self, x):
-        x = x.to(self._device)
-        for i in range(0, len(self._network_stack), 2):
-            batch_norm = getattr(self, self._network_stack[i + 1])
-            conv = getattr(self, self._network_stack[i])
-            x = self._activation_func(batch_norm(conv(x)))
-
-        return self._head(x.view(x.size(0), -1))
 
 
     def epsilon(self, A=0.3, B=0.1, C=0.1):
